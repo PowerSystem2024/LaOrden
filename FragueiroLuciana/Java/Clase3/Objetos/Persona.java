1da0e6aca9d9abc921aee7fe6224658ea2fc8444
Luciana Fragueiro <fragueiroluciana@gmail.com>
2024-08-31 19:12:41 -0300

    clase 1 2 3 java y clase 1 vue.js

---
+++ FragueiroLuciana/Java/Clase3/Objetos/Persona.java
@@ -0,0 +1,8 @@
+package Java.Clase3.Objetos;
+
+public class Persona {
+//atributos de la clase
+String nombre;
+String apellido;
+
+}
---
+++ FragueiroLuciana/Java/Clase3/ejc7.java
@@ -0,0 +1,23 @@
+/*ejercicio4: pedir numeros hasta que se teclee uno negativo,
+ * mostrar cuantos numeros se han introducido.
+ * Lo hacemos primero con la clase Scanner
+ * luego con la clase JOptions
+ */
+package Java.Clase3;
+import java.util.Scanner;
+public class ejc7 {
+public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    int numero, conteo = 0;
+    System.out.println("Digite un numero: ");
+    numero = Integer.parseInt(entrada.nextLine());
+    while(numero >= 0) {
+        System.out.println("El numero: "+numero+" es POSITIVO");
+        conteo++;
+        System.out.println("Digite otro numero: ");
+        numero = Integer.parseInt(entrada.nextLine());
+    }
+    System.out.println("Ingresaste "+conteo+" numeros que no son negativos");
+    entrada.close();
+}
+}
