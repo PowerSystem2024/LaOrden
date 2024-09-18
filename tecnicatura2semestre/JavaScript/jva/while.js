1e986dc613e5c68d2ffe0a1705f83ea5bdd87b29
santiago navarrete romero <santinavarrete54@gmail.com>
2024-09-18 18:59:58 -0300

    Clase 5

--- tecnicatura2semestre/JavaScript/jva/while.js
+++ tecnicatura2semestre/JavaScript/jva/while.js
@@ -155,8 +155,18 @@ let persona = {
     apellido: "Gil",
     email: "cgil@gmail.com",
     edad: 30,
+    idioma:"es",
+    get lang(){
+        return this.idioma.toUpperCase();
+    },
+    set lang(lang){
+        this.idioma = lang.toUpperCase();
+    },
     nombreCompleto: function(){
         return this.nombre + " " + this.apellido;
+    },
+    get nombreEdad(){
+        return "EL nombre es " + this.nombre + " edad: " + this.apellido;
     }
 }
 console.log(persona.nombre)
@@ -173,7 +183,7 @@ persona2.direccion = "Salada 14";
 persona2.telefono = "5492618282821";
 
 console.log(persona2.telefono)
-
+console.log("Creamos un nuevo objeto")
 console.log(persona["apellido"])
 
 //for in 
@@ -181,6 +191,93 @@ for(propiedad in persona){
     console.log(propiedad)
     console.log(persona[propiedad])
 }
+console.log("Cambiamos y eliminamos un error")
 persona.apellido = "Bentancud"
 delete persona.apellido;
 console.log(persona)
+
+console.log(persona.nombre+", "+ persona.apellido);
+
+for(nombrePropiedad in persona){
+    console.log(persona[nombrePropiedad]);
+}
+
+let  personaArray = Object.values(persona);
+console.log(personaArray);
+
+console.log("Distinta formas de imprimir un objeto: forma 3");
+let personaString = JSON.stringify(persona);
+console.log(personaString)
+
+console.log("Comenzamos a utilizar el metodo get");
+console.log(persona.nombreEdad);
+
+console.log("Comenzamos con el metodo get para idiomas");
+persona.lang = "en";
+console.log(persona.lang);
+
+function Persona3(nombre, apellido, email){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;   
+    this.nombreCompleto = function(){
+        return this.nombre + " " + this.apellido
+    }
+}
+let padre = new Persona3("Leo", "Lopez", "lopezl@gmail.com")
+this.nombre = "Luis" //modificamos el nombre
+padre.telefono = "5492618282821";
+console.log(padre)
+console.log(padre.nombreCompleto())
+
+let madre = new Persona3("Laura", "Contrera", "contrera@gmail.com");
+console.log(madre);
+console.log(madre.nombreCompleto())
+
+let miObjeto = new Object();
+
+let miObjeto2 = {}
+
+let miCadena1 = new String("Hola");
+
+let miCadena2 = "Hola";
+
+let miNumero = new Number(1);
+
+let miNumero2 = 1;
+
+let miBoolean1 = new Boolean(false);
+
+let miBoolean2 = false;
+
+let miArreglo1 = new Array();
+
+let miArreglo2 = [];
+
+let miFuncion1 = new Function();
+
+let miFuncion2 = function(){};
+
+Persona3.prototype.telefono = "2618383832"
+console.log(padre);
+console.log(madre.telefono)
+madre.telefono = "5492618383832"
+
+//Uso del call
+let persona4 = {
+    nombre: "Juan",
+    apellido: "Perez",
+    nombreCompleto2: function(titulo, telefono){
+        return titulo + ": " + this.nombre + " " + this.apellido + " "+ telefono
+    }
+}
+
+let persona5 = {
+    nombre: "Carlos", 
+    apellido: "Lara"
+}
+console.log(persona4.nombreCompleto2("Lic. ", "5492618484845"));
+console.log(persona4.nombreCompleto2.call(persona5, "Ing", "5492618585856"))
+
+let arreglo = ["Ing", "5492618686865"]
+console.log(persona4.nombreCompleto2.apply(persona5, arreglo))
