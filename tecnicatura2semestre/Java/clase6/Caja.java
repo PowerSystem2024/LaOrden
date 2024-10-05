bb37686220f9ce07894481ec50c8316676fc4d32
Nicolas Ortiz <nicolasortiz.it@gmail.com>
2024-10-05 14:49:42 -0300

    Clase 6 java

---
+++ tecnicatura2semestre/Java/clase6/Caja.java
@@ -0,0 +1,32 @@
+package clase6;
+
+/* 
+    Proyecto caja:
+    Ejercicio 1: Crear un proyecto según las especificaciones mostradas a continuación.
+    La fórmula es: volumen = ancho * alto * profundidad
+ */
+
+public class Caja {
+    public static void main(String[] args) {
+        // Atributos
+        int ancho;
+        int alto;
+        int profundo;
+
+        // Métodos y constructores
+        public Caja(){
+        }
+
+        // Constructor con parámetros
+        public Caja(int ancho, int alto, int profundo){ // Constructor 2
+            this.ancho = ancho;
+            this.alto = alto;
+            this.profundo = profundo;
+        } 
+
+        public int calcularVolumen(){ // Método para calcular
+            return ancho * alto * profundo;
+        }
+
+    }
+}
